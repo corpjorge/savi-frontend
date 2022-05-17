26c437414aed40d8c52a8b8c39a27f6aaaebc1e1
Jorge Eduardo <corpjorge@hotmail.com>
2022-05-16 19:37:08 -0500

    feat: tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-
-import "./assets/global.css";
+import "./assets/styles/global.css";
 
 const app = createApp(App);
 
