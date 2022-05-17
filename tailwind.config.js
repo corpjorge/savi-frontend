f20acdd6895cd70cad6aab4577dd750b9dd09f25
Jorge Eduardo <corpjorge@hotmail.com>
2022-05-16 21:44:59 -0500

    feat: colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      heading: ["Roboto", "sans-serif"],
+    },
     colors: {
       green: {
         100: "#e6ffed",
