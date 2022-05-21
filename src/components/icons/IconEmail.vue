c0905d29effda1009abba7483c7696254ea85e58
Jorge Eduardo <corpjorge@hotmail.com>
2022-05-21 14:00:33 -0500

    feat: diseño vista verificar email

---
+++ src/components/icons/IconEmail.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+defineProps<{
+  className?: string;
+  width?: number;
+  height?: number;
+  fill?: string;
+  stroke?: string;
+  id?: string;
+}>();
+</script>
+<template>
+  <svg
+    :class="className"
+    :width="width"
+    :height="height"
+    viewBox="0 0 512 512"
+    style="enable-background: new 0 0 512 512"
+  >
+    <circle style="fill: #93ffd0" cx="256" cy="256" r="256" />
+    <path
+      style="fill: #62b390"
+      d="M283.026,510.581C402.823,498.01,498.01,402.823,510.581,283.026L398.222,170.667l-64.414,137.138
+	l-220.031,33.528L283.026,510.581z"
+    />
+    <rect
+      x="113.778"
+      y="170.667"
+      style="fill: #008d68"
+      width="284.444"
+      height="170.667"
+    />
+    <rect
+      x="255.431"
+      y="170.667"
+      style="fill: #036a4e"
+      width="142.791"
+      height="170.667"
+    />
+    <polygon
+      style="fill: #06ab80"
+      points="398.222,170.667 256,256 113.778,170.667 "
+    />
+    <polygon
+      style="fill: #02a179"
+      points="398.222,170.667 255.426,170.667 255.426,255.655 256,256 "
+    />
+  </svg>
+</template>
--- src/views/VerifyEmailView.vue
+++ src/views/VerifyEmailView.vue
@@ -1,15 +1,40 @@
+<script setup lang="ts">
+import IconEmail from "@/components/icons/IconEmail.vue";
+import SButton from "@/components/SaviUI/S-Button.vue";
+</script>
 <template>
-  <div class="check-mail">
-    <div class="check-mail__title">
-      <h1>ddd}</h1>
-    </div>
-    <div class="check-mail__content">
-      <div class="check-mail__content-text">
-        <p>dd}}</p>
+  <div class="mt-24 mx-5">
+    <div class="justify-center text-center content-center items-center">
+      <IconEmail class-name="inline " :width="160" :height="160" />
+      <div class="flex justify-center text-center">
+        <h1 class="mt-14 text-gray-800 text-4xl font-semibold text-center">
+          Verifica tu correo electrónico
+        </h1>
       </div>
-      <div class="check-mail__content-button">
-        <button class="button" @click="goToLogin">cdcdc</button>
+      <div class="flex justify-center text-center">
+        <p class="my-6 lg:w-[44rem] text-gray-600 text-center">
+          Hemos enviado un correo electrónico a
+          <span class="font-semibold">xxx</span> con un enlace para verificar tu
+          cuenta. Recuerda también validar tu bandeja de correos no deseados o
+          Spam
+        </p>
       </div>
+
+      <div class="flex justify-center items-center">
+        <div class="w-80">
+          <SButton
+            type="submit"
+            styling="success"
+            text="Renviar mensaje"
+            typeof="submit"
+            :disabled="true"
+          />
+        </div>
+      </div>
+
+      <p class="mt-5 text-center text-gray-500 text-xs">
+        &copy;2022 SAVI. All rights reserved.
+      </p>
     </div>
   </div>
 </template>
