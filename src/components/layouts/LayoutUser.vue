938b3272ebceb7857b3645315ffacba3cadae36b
Jorge Eduardo <corpjorge@hotmail.com>
2022-05-23 19:01:01 -0500

    fix: remove conosle log

--- src/components/layouts/LayoutUser.vue
+++ src/components/layouts/LayoutUser.vue
@@ -4,7 +4,6 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 const user = JSON.parse(localStorage.getItem("user") || "{}");
 
-console.log(user);
 if (user.type <= 3) {
   router.push({ name: "Home-Admin" });
 }
