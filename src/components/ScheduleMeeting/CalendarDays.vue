d2733faafa9515e1d27572fce5a0c58aaf7b26df
Jorge Eduardo <corpjorge@hotmail.com>
2022-06-05 19:21:26 -0500

    refactor: validateDays.ts

--- src/components/ScheduleMeeting/CalendarDays.vue
+++ src/components/ScheduleMeeting/CalendarDays.vue
@@ -2,7 +2,7 @@
 import { onMounted, ref } from "vue";
 import LoaderComponent from "@/components/LoaderComponent.vue";
 import { useMeetingsMonths } from "@/hooks/useMeetingsMonths";
-import { HOLIDAYS } from "@/utils/holidays";
+import { validateDays } from "@/utils/validateDays";
 
 const meetings = ref<any>();
 
@@ -33,28 +33,14 @@ defineProps<{
           (currentDay === day && Number(month) === currentMonth
             ? 'text-blue-500 border-blue-500 cursor-pointer'
             : 'text-gray-600') +
-          (currentDay > day && Number(month) === currentMonth ||
-           meetings.filter((date: any) => { return date.month === month && date.day === day }).length >= 10 ||
-           index === 'DO' ||
-           HOLIDAYS.some((holiday: any) => { return holiday.month === month && holiday.day === day; }) ||
-           (currentDay === day &&  currentMonth === Number(month) && new Date().getHours() >= 17)
+          (validateDays(day, month, currentDay, currentMonth, meetings, index)
             ? ' text-gray-400 cursor-no-drop'
             : ' cursor-pointer hover:border-blue-500') +
           (day === 0 ? ' border-none mb-[2.35rem]' : '')
         "
         @click="
-          (currentDay > day && Number(month) === currentMonth) ||
-          meetings.filter((date: any) => {
-            return date.month === month && date.day === day;
-          }).length >= 10 ||
-          index === 'DO' ||
-          HOLIDAYS.some((holiday: any) => {
-            return holiday.month === month && holiday.day === day;
-          }) ||
-          (currentDay === day &&
-            currentMonth === Number(month) &&
-            new Date().getHours() >= 17)
-            ? ''
+          validateDays(day, month, currentDay, currentMonth, meetings, index)
+            ? null
             : daySelect(day)
         "
       >
---
+++ src/utils/validateDays.ts
@@ -0,0 +1,44 @@
+import { HOLIDAYS } from "@/utils/holidays";
+
+export function validateDays(
+  day: number,
+  month: number,
+  currentDay: number,
+  currentMonth: number,
+  meetings: any,
+  index: any
+) {
+  function pastDays() {
+    return currentDay > day && Number(month) === currentMonth;
+  }
+
+  function busyDay() {
+    return (
+      meetings.filter((date: any) => {
+        return date.month === month && date.day === day;
+      }).length >= 10
+    );
+  }
+
+  function isHolyDay() {
+    return HOLIDAYS.some((holiday: any) => {
+      return holiday.month === month && holiday.day === day;
+    });
+  }
+
+  function hourNotAvailable() {
+    return (
+      currentDay === day &&
+      currentMonth === Number(month) &&
+      new Date().getHours() >= 17
+    );
+  }
+
+  return (
+    pastDays() ||
+    busyDay() ||
+    index === "DO" ||
+    isHolyDay() ||
+    hourNotAvailable()
+  );
+}
