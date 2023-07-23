eba39b2d4708da0e2f35b9eb14da5f6aaaebda69
j1guzman <j1guzman@fundaciongruposocial.co>
2023-07-23 00:08:17 -0500

    dist

--- src/utils/VerifyValidEmailView.ts
+++ src/utils/VerifyValidEmailView.ts
@@ -5,16 +5,9 @@ export function VerifyValidEmailView() {
   const urlParams = new URLSearchParams(urlWindow);
   let urlBack = urlParams.get("verify_url") as string;
 
-  // Decodifica la URL
   urlBack = decodeURIComponent(urlBack);
 
-  // Obtiene la cadena después del primer '?'
-  const queryString = urlBack.split("?")[1];
-
-  // Divide la cadena en '/'
-  const data = queryString.split("/");
-
-  http.get(`email/verify/${data[2]}/${data[3].split("?")[0]}`).then(() => {
+  http.get(urlBack).then(() => {
     window.location.href = "/";
   });
 }
