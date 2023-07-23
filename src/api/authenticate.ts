1b7d2f3b36c5da96abd1125a37ade731afb51d7d
j1guzman <j1guzman@fundaciongruposocial.co>
2023-07-22 22:26:47 -0500

    savi update

--- src/api/authenticate.ts
+++ src/api/authenticate.ts
@@ -2,6 +2,6 @@ import http from "@/helpers/http";
 import axios from "axios";
 
 export const authenticate = async (credentials: object) => {
-  await axios.get(`${import.meta.env.VITE_APP_URL}sanctum/csrf-cookie`);
+  await axios.get(`${import.meta.env.VITE_APP_URL}/sanctum/csrf-cookie`);
   return await http.post("login", credentials);
 };
